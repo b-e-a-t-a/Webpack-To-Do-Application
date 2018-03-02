d847ce6f1f4ffa4610be69f3b84d069058076e46
Beata Karolczak <karolczak.beata@gmail.com>
2018-03-02 12:28:47 +0100

    Add remove

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import style from './TodoList.css';
 
-const TodoList = props => <ul className={style.TodoList}>{props.list.map(data => <li key={data.id}> {data.text} </li>)} 
+const Todo = ({ item, removeItem }) => {
+	return <li onClick = {() => removeItem(item.id)}> {item.text} </li>;
+};
 
-						</ul>
+const TodoList = ({ list, remove }) => {
+  const todoList = list.map(data => {
+    return <Todo item={data} removeItem={remove} key={data.id} />;
+  });
 
-//onClick = {() => this.props.removeTodo(todo.id)}
+  return <ul className={style.TodoList}> {todoList} </ul>;
+};
 
 export default TodoList;
+
+// const TodoList = props => <ul className={style.TodoList}> {props.list.map(data => <li key={data.id}> {data.text} </li>)} </ul>
+//onClick = {() => this.props.removeTodo(todo.id)}
